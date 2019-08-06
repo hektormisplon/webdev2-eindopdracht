75080185cc4a8f7541b2bf795888907b01b71e28
hektormisplon <hektor.misplon@gmail.com>
2019-08-06 17:27:40 +0200

    linting

--- laravel/app/Http/Controllers/ProjectPledgeController.php
+++ laravel/app/Http/Controllers/ProjectPledgeController.php
@@ -8,28 +8,32 @@ use App\Project;
 
 class ProjectPledgeController extends Controller
 {
-    public function index() 
+    public function index()
     {
         $pledges = Pledge::all();
         return view('pledges.index', compact('pledges'));
     }
 
-    public function create() {
+    public function create()
+    {
         $pledged = Pledge::create(request()->validate([
             'pledged' => ['required'],
             'amount' => ['required']
         ]));
     }
 
-    public function show(Pledge $pledge) {
+    public function show(Pledge $pledge)
+    {
         return view('pledges.show', compact('pledge'));
     }
 
-    public function store(Project $project) {
+    public function store(Project $project)
+    {
         $project->addPledge(request('amount'));
     }
 
-    public function update(Pledge $pledge) {
+    public function update(Pledge $pledge)
+    {
         $pledge->sponsor();
         return back();
     }
