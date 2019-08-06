b66c5d0eee0794a9b5f2c0ea418790b69518b134
hektormisplon <hektor.misplon@gmail.com>
2019-08-06 17:27:26 +0200

    phpunit tests

--- laravel/tests/Feature/ProjectTest.php
+++ laravel/tests/Feature/ProjectTest.php
@@ -10,19 +10,55 @@ class ProjectTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function createProjectAsNonVerifiedUser()
+    {
+        // Given activated user
+        $this->actingAs(factory('App\User', 'nonVerified')->create());
+
+        // When passing required data to creation endpoint 
+        $attributes = [
+            'title' => 'Non verified user project',
+            'description' => 'Project created by non verified user.'
+        ];
+
+        $this->post(route('projects.store'), $attributes);
+
+        // Then new project in db
+        $this->assertDatabaseMissing('projects', $attributes);
+    }
+
     /** @test */
-    public function createProjectAsActivatedUser() {
+    public function createProjectAsVerifiedUser()
+    {
+        $this->withoutExceptionHandling();
 
         // Given activated user
-        $this->actingAs(factory('App\User')->create());
+        $this->actingAs(factory('App\User', 'verified')->create());
 
-        // When passing required data to projects/create endpoint 
+        // When passing required data to creation endpoint 
         $attributes = [
             'title' => 'Project title',
             'description' => 'Lorem ipsum dolor sit.'
         ];
-        auth()->user()->isVerified() 
-            ? $this->post(route('projects.store'), $attributes) 
+
+        $this->post(route('projects.store'), $attributes);
+
+        // Then new project in db
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+
+    public function createProjectAsAdmin()
+    {
+        // Given activated user
+        $this->actingAs(factory('App\User', 'admin')->create());
+
+        // When passing required data to creation endpoint 
+        $attributes = [
+            'title' => 'Project title',
+            'description' => 'Lorem ipsum dolor sit.'
+        ];
+        auth()->user()->hasVerifiedEmail()
+            ? $this->post(route('projects.store'), $attributes)
             : redirect('/home');
 
         // Then new project in db
