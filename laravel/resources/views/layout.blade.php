a08ab9f2fbfbb74a265465ba7884d52afd151a51
hektormisplon <hektor.misplon@gmail.com>
2019-08-08 19:59:07 +0200

    styling

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', config('app.name'))</title>
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=DM+Sans:400,400i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:700,400,400i&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="https://unpkg.com/feather-icons"></script>
@@ -58,22 +58,21 @@
     </style>
 </head>
 
-<body>
+<body class="layout-container">
     @include('partials.header')
-    <div class="container of-hidden">
+    <div class="content-container">
         <div class="columns">
             @auth
             <div class="column col-md-1 col-lg-3 col-xl-3 col-2">
                 @include('partials.sidebar')
             </div>
             @endauth
-            <div class="column col-md-10 col-lg-8 col-xl-8 col-4 col-1-mx-auto">
-                <div class="container m-header">
-                    @yield('content')
-                </div>
+            <div class="column col-md-10 col-lg-8 col-xl-8 col-4 col-1-mx-auto @guest p-centered @endguest">
+                @yield('content')
             </div>
         </div>
     </div>
+    @include('partials.footer')
     <script>
         feather.replace()
     </script>
--- laravel/resources/views/partials/sidebar.blade.php
+++ laravel/resources/views/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="off-canvas off-canvas-sidebar-show">
-    <a class="off-canvas-toggle btn btn-primary btn-action m-header" href="#sidebar">
+    <a class="off-canvas-toggle btn btn-primary btn-action" href="#sidebar">
         <i class="icon icon-arrow-right"></i>
     </a>
     <div class="off-canvas-sidebar bg-primary m-header" id="sidebar">
@@ -8,7 +8,7 @@
                 <a class="text-light" href="/projects">Projects</a>
                 <ul class="nav">
                     <li class="nav-item">
-                        <a href="/projects/create">Create project</a>
+                        <a class="text-light" href="/projects/create">Create project</a>
                     </li>
                 </ul>
             </li>
