5a8b7b382c04d885fb92ad85991682983825ac05
hektormisplon <hektor.misplon@gmail.com>
2019-08-08 19:53:00 +0200

    comments

---
+++ laravel/database/migrations/2019_08_07_093819_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->text('message');
+            $table->unsignedInteger('sender_id');
+            $table->unsignedInteger('project_id');
+            $table->timestamps();
+
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            Schema::dropIfExists('comments');
+        });
+    }
+}
