d55dd6fed34c0dd20abbb03722cf9d4693446960
hektormisplon <hektor.misplon@gmail.com>
2019-08-09 18:52:31 +0200

    news page

--- laravel/resources/views/news.blade.php
+++ laravel/resources/views/news.blade.php
@@ -1,15 +1,30 @@
 @extends('layout')
 @section('title', 'What\'s new?')
 @section('content')
-<div>
-    <h3>News</h3>
-    <div class="divider" data-content="Latest news"></div>
-    <div class="card">
-        <div class="card-header">
-            <h6 class="card-title">Article</h6>
+<h3>News</h3>
+<div class="divider" data-content="Latest news"></div>
+<div class="article-container" style='height: 75vh'>
+    @foreach ($news as $article)
+    <div class="tile tile-centered">
+        <div class="tile-content">
+            <h6 class="card-title">{{ $article->title }}</h6>
+            <em>{{ $article->author }}</em>
+            <small class="text-gray float-right">{{ $article->created_at->format('M j Y | h:m:s') }}</small>
             <div class="divider"></div>
-            <div class="card-subtitle">Description of the article</div>
+            <div class="card-subtitle">{{ $article->description }}</div>
+            <div class="divider"> </div>
+            <div class="btn-group">
+                <a href="#" class="btn">Read</a>
+                <!-- TODO: route('news/{id}') -->
+            </div>
+            <div class="btn-group float-right">
+                <a class="btn btn-link"><i data-feather="heart"></i></a>
+            </div>
         </div>
     </div>
+    @endforeach
+</div>
+<div class="div">
+    {{ $news->links() }}
 </div>
 @endsection
---
+++ laravel/resources/views/news/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+<h3>News</h3>
+
+@endsection
