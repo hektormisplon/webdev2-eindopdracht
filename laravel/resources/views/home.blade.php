9cae4af549c9dc1ac038407b743e0f665611033f
hektormisplon <hektor.misplon@gmail.com>
2019-08-17 13:13:26 +0200

    refactor to model

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <div class="card">
     <div class="card-header">
         <h3>
-            @if(auth()->user()->role == 'admin')
+            @if(auth()->user()->isAdmin())
             <i class="icon icon-3x" data-feather="zap"></i>
             @endif
             {{ auth()->user()->email }}
