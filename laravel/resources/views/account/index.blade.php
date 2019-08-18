7fa47759b3cc72520c473d46ec6ceca6090a00d3
hektormisplon <hektor.misplon@gmail.com>
2019-08-18 23:58:28 +0200

    show user accounts

--- laravel/resources/views/account/index.blade.php
+++ laravel/resources/views/account/index.blade.php
@@ -1,14 +1,12 @@
 @section('title', 'Create project')
 @extends('layout')
 @section('content')
-<div>
-    <h3 class="text-bold">Accounts overview</h3>
-    @foreach($users as $user)
-    <div class="tile tile-centered">
-        <p>{{ $user->email }}</p>
-    </div>
-    @endforeach
+<h3 class="text-bold">Accounts overview</h3>
+@foreach($users as $user)
+<div class="tile tile-centered">
+    <p>{{ $user->email }}</p>
 </div>
+@endforeach
 <div class="div">
     {{ $users->links() }}
 </div>
