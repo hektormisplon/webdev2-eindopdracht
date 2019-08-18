d168c167cc19899f01d1e456ff91d8205e49b5ed
hektormisplon <hektor.misplon@gmail.com>
2019-08-18 23:53:25 +0200

    home markup & styling

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,15 +1,67 @@
 @extends('layout')
 @section('content')
-<div class="card">
-    <div class="card-header">
-        <h3>
-            @if(auth()->user()->isAdmin())
-            <i class="icon icon-3x" data-feather="zap"></i>
-            @endif
-            {{ auth()->user()->email }}
-        </h3>
-    </div>
-    <div class="card-body">
-    </div>
+<h3 class="text-bold">
+    Hello there,
+</h3>
+<h5>
+    @if(auth()->user()->isAdmin())
+    <i class="icon icon-2x" data-feather="zap"></i>
+    @endif
+    {{ auth()->user()->email }}
+</h5>
+<div class="divider" data-content="What would you like to do?"></div>
+<div class="d-flex">
+    <a href="/projects">
+        <div class="tile tile-centered">
+            <div class="tile-icon">
+                <i class="icon icon-2x" data-feather="briefcase"></i>
+            </div>
+            <div class="tile-content">
+                <div class="tile-title">My projects</div>
+            </div>
+            <div class="tile-action">
+            </div>
+        </div>
+    </a>
+    
+    <a href="/credits">
+        <div class="tile tile-centered">
+            <div class="tile-icon">
+                <i class="icon icon-2x" data-feather="package"></i>
+            </div>
+            <div class="tile-content">
+                <div class="tile-title">My creunits</div>
+            </div>
+            <div class="tile-action">
+            </div>
+        </div>
+    </a>
+    
+    <a href="/discover">
+        <div class="tile tile-centered">
+            <div class="tile-icon">
+                <i class="icon icon-2x" data-feather="wind"></i>
+            </div>
+            <div class="tile-content">
+                <div class="tile-title">Discover</div>
+            </div>
+            <div class="tile-action">
+            </div>
+        </div>
+    </a>
+
+    <a href="/projects/create">
+        <div class="tile tile-centered">
+            <div class="tile-icon">
+                <i class="icon icon-2x" data-feather="plus"></i>
+            </div>
+            <div class="tile-content">
+                <div class="tile-title">New project</div>
+            </div>
+            <div class="tile-action">
+            </div>
+        </div>
+    </a>
 </div>
-@endsection
+    
+    @endsection
