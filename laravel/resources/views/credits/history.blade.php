ed03d5084c7a41d62ae0a9040c0068ffaa21c17e
hektormisplon <hektor.misplon@gmail.com>
2019-08-19 18:04:18 +0200

    pledge history

---
+++ laravel/resources/views/credits/history.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+<h3 class="text-bold">Pledge history</h3>
+<div class="panel">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Order amount</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($transactions as $transaction)                
+            <tr class="active">
+                <td>{{ $transaction->credit_amount }}</td>
+                <td>{{ $transaction->created_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+</div>
+@endsection
