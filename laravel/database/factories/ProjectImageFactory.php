c6ab9b521e68b8c798d9e1fe54dca0123918347c
hektormisplon <hektor.misplon@gmail.com>
2019-08-19 23:10:11 +0200

    placeholder images category & size

--- laravel/database/factories/ProjectImageFactory.php
+++ laravel/database/factories/ProjectImageFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(ProjectImage::class, function (Faker $faker) {
     return [
-        'filepath' => $faker->imageUrl($width = 640, $height = 480),
-        'filename' => $faker->unique()->word(),
+        'filepath' => $faker->imageUrl($width = 1200, $height = 900, 'technics'),
+        'filename' => '',
         'project_id' => $faker->numberBetween($min = 1, $max = 10)
     ];
 });
