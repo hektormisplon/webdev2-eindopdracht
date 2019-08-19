e29094e76cec258a153baad0d34f0fc14ea33d86
hektormisplon <hektor.misplon@gmail.com>
2019-08-19 19:32:51 +0200

    fund project admin fee & validation

--- laravel/app/Http/Controllers/ProjectPledgeController.php
+++ laravel/app/Http/Controllers/ProjectPledgeController.php
@@ -10,12 +10,29 @@ class ProjectPledgeController extends Controller
 {
     public function pledge(Project $project)
     {
+
+        $user = auth()->user();
+        if ($project->owner->id == $user->id) {
+            return redirect('discover/details/' . $project->id)->with('warning', 'The goal is to find other people to fund your project.');
+        }
+
         Transaction::create([
             'credit_amount' => request('pledged'),
             'project_id' => $project->id,
             'user_id' => auth()->user()->id
         ]);
-        $project->update(['pledged' => $project->pledged + request('pledged')]);
-        return redirect('discover/details/' . $project->id)->with('message', 'You pledged ' . request('pledged') . ' creunits');
+
+        $adminFee = request('pledged') * .1;
+        $admin = \App\User::where('role', 'admin')->first();
+        $admin->update(['credit_amount' => $admin->credit_amount + $adminFee]);
+
+        $pledged = request('pledged') - $adminFee;
+        $user_balance = $user->credit_amount;
+        if ($user_balance < request('pledged')) {
+            return redirect('discover/details/' . $project->id)->with('error', 'You don\'t  have enough creunits for that. ');
+        }
+        $user->update(['credit_amount' => $user->credit_amount - request('pledged')]);
+        $project->update(['pledged' => $project->pledged + $pledged]);
+        return redirect('discover/details/' . $project->id)->with('message', 'You pledged ' . $pledged . ' creunits');
     }
 }
